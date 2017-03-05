d6b0831ac74d4671ba3e08620ff7d321e8a2beb6
路云飞 <luyunfeicx@163.com>
2017-03-06 02:19:47 +0800

    fix animation bug

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Players { Left, Right }
+
+public enum Stuffs { Shit, Fish, Sword, Ball }
+
+public class Player : MonoBehaviour {
+
+
+    private Animator anim;
+	private float myForce;
+	private Stuffs myStuff;
+    public int HP = 10;
+    public GameObject shitPrefab;
+    public GameObject fishPrefab;
+    public GameObject swordPrefab;
+    public GameObject ballPrefab;
+
+    public Players who;
+
+    public Transform thrower;
+
+    public Vector2 direction = new Vector2(1f, 1f);
+    public float forceMultiplier;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+	public void ThrowIt()
+	{
+		// 扔东西
+		Debug.LogFormat("Player {0} throws stuff with force {1}", (who == Players.Left) ? 1 : 2, myForce);
+		GameObject stuffPrefab;
+		switch (myStuff)
+		{
+		case Stuffs.Ball: stuffPrefab = ballPrefab; break;
+		case Stuffs.Fish: stuffPrefab = fishPrefab; break;
+		case Stuffs.Sword: stuffPrefab = swordPrefab; break;
+		case Stuffs.Shit: stuffPrefab = shitPrefab; break;
+		default: stuffPrefab = null; break;
+		}
+
+		Instantiate<GameObject>(stuffPrefab, thrower.transform.position, Quaternion.identity)
+			.GetComponent<Rigidbody2D>().AddForce(direction * ((myForce + 0.1f) / 1.1f * forceMultiplier));
+	}
+
+    public void ThrowStuff(float force, Stuffs stuff)
+    {
+		myForce = force;
+		myStuff = stuff;
+
+        // 动画
+        anim.SetTrigger("Attack");
+    }
+
+    public void TakeDamage(int damage)
+    {
+		anim.SetTrigger("Hurt");
+        HP -= damage;
+        UIManager.instance.UpdateHP(Mathf.Max(0, HP), who);
+        if (HP <= 0)
+        {
+            anim.SetTrigger("Die");
+        }
+        //else
+        //   anim.SetTrigger("Hurt");
+    }
+}
---
+++ Assets/Throw.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : StateMachineBehaviour {
+
+	private bool thrown = false;
+	private string name = "XianYu";
+	private float time = 7f / 9;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		thrown = false;
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if (!thrown) {
+			GameManager gm = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+			if (gm.fsm.State == GameManager.States.Left) {
+				name = "WangWang";
+				time = 3f / 7;
+			}
+			if (stateInfo.normalizedTime >= time) {
+				GameObject.Find (name).GetComponent<Player> ().ThrowIt ();
+				thrown = true;
+			}
+
+		}
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
